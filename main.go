1e1fb37fdca2c4aa970d4f545915f13c5633e99c
karasawa <pain.keita.27@gmail.com>
2023-01-06 18:33:35 +0900

    add user, todo model

--- main.go
+++ main.go
@@ -9,10 +9,35 @@ func main() {
 
 	fmt.Println(models.Db)
 
-	u := &models.User{}
-	u.Name = "test"
-	u.Email = "test@gmail.com"
-	u.Password = "testtest"
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@gmail.com"
+	// u.Password = "testtest"
 
-	u.CreateUser()
+	// u.CreateUser()
+
+	// u, _ := models.GetUser(1)
+	// fmt.Println(u)
+
+	// u.DeleteUser()
+
+	// u.Name = "test2"
+	// u.Email = "test2@gmail.com"
+	// u.UpdateUser()
+
+	// u, _ = models.GetUser(1)
+	// fmt.Println(u)
+
+	// u := &models.User{}
+	// u.Name = "test"
+	// u.Email = "test@gmail.com"
+	// u.Password = "testtest"
+
+	// u.CreateUser()
+
+	u, _ := models.GetUser(2)
+	fmt.Println(u)
+
+	u.CreateTodo("掃除")
+
 }
