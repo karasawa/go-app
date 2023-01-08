ae02f35ef4b0f31eb0d16c63b5fc8fb39f5201d6
karasawa <pain.keita.27@gmail.com>
2023-01-08 12:04:13 +0900

    add server.go

--- main.go
+++ main.go
@@ -2,42 +2,11 @@ package main
 
 import (
 	"fmt"
+	"go-app/app/controllers"
 	"go-app/app/models"
 )
 
 func main() {
-
 	fmt.Println(models.Db)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@gmail.com"
-	// u.Password = "testtest"
-
-	// u.CreateUser()
-
-	// u, _ := models.GetUser(1)
-	// fmt.Println(u)
-
-	// u.DeleteUser()
-
-	// u.Name = "test2"
-	// u.Email = "test2@gmail.com"
-	// u.UpdateUser()
-
-	// u, _ = models.GetUser(1)
-	// fmt.Println(u)
-
-	// u := &models.User{}
-	// u.Name = "test"
-	// u.Email = "test@gmail.com"
-	// u.Password = "testtest"
-
-	// u.CreateUser()
-
-	u, _ := models.GetUser(2)
-	fmt.Println(u)
-
-	u.CreateTodo("掃除")
-
+	controllers.StartMainServer()
 }
